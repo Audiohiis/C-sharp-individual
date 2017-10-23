047d0aa56047c0e0c1f4beabba363de54e46c31e
Hedi T <heditamm.th@gmail.com>
2017-10-23 15:40:05 +0300

    Foreach

---
+++ src/01-exercises/Exercises/09-foreach-array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace foreacharray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutisse", "Paha oli olla" };
+
+            int i = 1;
+
+            foreach (string excuse in excuses)
+            {
+                Console.WriteLine(i + "." + excuse);
+                i++;
+            }
+        }
+    }
+}
