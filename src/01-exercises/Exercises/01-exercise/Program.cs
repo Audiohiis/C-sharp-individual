c92df7cdd9476a76d17c2e27d5dc8ce7883df0fb
Hedi T <heditamm.th@gmail.com>
2017-10-23 13:58:32 +0300

    Add Hello World

---
+++ src/01-exercises/Exercises/01-exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_Hello_World
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jou");
+        }
+    }
+}
