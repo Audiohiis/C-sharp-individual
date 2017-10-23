d5e80aad4d1ef9ed81881503cf6044ce053d2c2c
Hedi T <heditamm.th@gmail.com>
2017-10-23 14:13:49 +0300

    Add more lines

---
+++ src/01-exercises/Exercises/02-input/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace input
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Minu nimi on (sisesta oma nimi) ja ma õpin progema");
+            Console.WriteLine("Kindlasti saab midagi rohkemat teha");
+        }
+    }
+}
