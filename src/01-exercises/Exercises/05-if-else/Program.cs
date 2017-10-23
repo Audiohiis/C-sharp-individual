06e31a0279c3e91928b36391308b1fe111259819
Hedi T <heditamm.th@gmail.com>
2017-10-23 15:19:36 +0300

    Ask input number and check if-else conditions

---
+++ src/01-exercises/Exercises/05-if-else/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ifelse
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri [1-100]. Proovi see ära arvata!");
+
+            Random rnd = new Random();
+            int myNumber = rnd.Next(1, 10);
+
+            Console.Write("Sina pakud: ");
+            int userNumber = Int32.Parse(Console.ReadLine());
+
+            if (myNumber > userNumber)
+            {
+                Console.WriteLine("Minu number on suurem.");
+            }
+            else if  (myNumber < userNumber) {
+                Console.WriteLine("Minu number on väiksem");
+            }
+            else {
+                Console.WriteLine("Ära arvasid!");
+            }
+               
+        }
+    }
+}
