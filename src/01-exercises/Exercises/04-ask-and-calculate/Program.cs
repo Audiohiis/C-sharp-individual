4ee1c485f522500231deedfd0456efcdbb8f1317
Hedi T <heditamm.th@gmail.com>
2017-10-23 14:38:06 +0300

    Ask input and calculate

---
+++ src/01-exercises/Exercises/04-ask-and-calculate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace askandcalculate
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaator. Ütle mulle kaks numbrit ja ma ütlen sulle nende summa");
+            Console.Write("Esimene number: ");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Teine number: ");
+            int secondNumber = Int32.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+            Console.Write($"Tulemuseks on: {sum}");
+        }
+    }
+}
