9ce0b188a31f970a560d6fa5f762b5ec95a79da9
Hedi T <heditamm.th@gmail.com>
2017-10-23 14:23:37 +0300

    Ask user input

---
+++ src/01-exercises/Exercises/03-user-input/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace userinput
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen papagoi programm. Ütle mulle midagi ja ma kordan seda");
+
+            Console.Write("Sina ütled: ");
+            string askInput = Console.ReadLine();
+            Console.Write($"Mina ütlen: {askInput}");
+
+        }
+    }
+}
