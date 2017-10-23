0535823693a9b9e1f216a9b0d7adf66f788c016f
Hedi T <heditamm.th@gmail.com>
2017-10-23 15:31:08 +0300

    Print array elements

---
+++ src/01-exercises/Exercises/08-excuses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace excuses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutisse", "Paha oli olla" };
+
+            for (int i = 0; i < excuses.Length; i++) {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+            }
+        }
+    }
+}
