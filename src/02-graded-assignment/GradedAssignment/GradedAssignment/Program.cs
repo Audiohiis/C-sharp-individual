367dd1b1ef14050a23f25274eaf24a45a9eea570
Hedi T <heditamm.th@gmail.com>
2017-10-25 12:46:30 +0300

    Graded assignment

---
+++ src/02-graded-assignment/GradedAssignment/GradedAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradedAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the amount: ");
+            int sum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Your sum is: {sum}$");
+
+            if (sum >= 50) {
+                //5%
+            }
+        }
+    }
+}
